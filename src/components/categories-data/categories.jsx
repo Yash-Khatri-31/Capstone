aec91369baed50729f42f0d01202576728614c46
Yash-Khatri-31 <yashr124@gmail.com>
2023-01-05 16:49:30 +0400

    updating directory

---
+++ src/components/categories-data/categories.jsx
@@ -0,0 +1,29 @@
+const Categories = [
+    {
+        "id": 1,
+        "title": "Hats",
+        "imageUrl": "https://www.freepnglogos.com/uploads/pirate-png/pirate-hat-cliparts-download-clip-art-clip-art-clipart-library-23.png"
+    },
+    {
+        "id": 2,
+        "title": "Jackets",
+        "imageUrl": "https://www.freepnglogos.com/uploads/jacket-png/red-suede-jacket-club-penguin-wiki-fandom-powered-wikia-31.png"
+    },
+    {
+        "id": 3,
+        "title": "Shoes",
+        "imageUrl": "https://www.freepnglogos.com/uploads/shoes-png/shoes-clipart-clipart-images-clipartwork-15.png"
+    },
+    {
+        "id": 4,
+        "title": "Women",
+        "imageUrl": "https://png.pngitem.com/pimgs/s/498-4985935_anime-girl-with-brown-hair-and-glasses-hd.png"
+    },
+    {
+        "id": 5,
+        "title": "Men",
+        "imageUrl": "https://i.pinimg.com/736x/71/92/39/719239b9db26294c0177d8b43c8bbc43.jpg"
+    }
+]
+
+export default Categories;
