8fb033ed745d6ba14812435b5b2277fa6e78f7f3
Yash-Khatri-31 <yashr124@gmail.com>
2023-03-05 16:32:33 +0530

    added selector

--- src/routes/navigation/navigation.route.jsx
+++ src/routes/navigation/navigation.route.jsx
@@ -1,8 +1,9 @@
 import { useContext } from 'react';
 import { Outlet } from "react-router-dom";
-import { UserContext } from '../../contexts/user.context';
+import { useSelector } from 'react-redux';
 import { CartContext } from '../../contexts/cart.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
+import { selectCurrentUser } from '../../store/user/user.selector';
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
@@ -10,7 +11,7 @@ import { NavigationContainer, LogoContainer, NavLinksContainer, NavLink } from '
 
 const Navigation = () => {
 
-    const { currentUser } = useContext(UserContext);
+    const currentUser = useSelector(selectCurrentUser)
     const { toggle } = useContext(CartContext);
 
     return (
@@ -38,3 +39,7 @@ const Navigation = () => {
 };
 
 export default Navigation;
+
+// useSelector, we pass a selector function. Extracts the values we want from the entire redux store. We recieve the entire state object.
+// for user it will be like state.user
+// 
